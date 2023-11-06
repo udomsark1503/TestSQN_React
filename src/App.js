44a29355cdaacfb0c372842d71b78cfd72544e15
Udomsark Amnuaysookwong <udomsark1863@hotmail.com>
2023-11-06 22:03:32 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { Col, Row } from "antd";
+import ReactApexChart from "react-apexcharts";
+import axios from "axios";
 
-function App() {
+const App = () => {
+  const [chartData, setChartData] = useState({
+    series: [
+      {
+        data: [],
+      },
+    ],
+    options: {
+      chart: {
+        type: "bar",
+        height: 350,
+      },
+      plotOptions: {
+        bar: {
+          borderRadius: 4,
+          borderRadiusApplication: "end",
+          horizontal: true,
+        },
+      },
+      dataLabels: {
+        enabled: false,
+      },
+      xaxis: {
+        categories: [],
+      },
+    },
+  });
+  const [year, setYear] = useState(1950);
+  useEffect(() => {
+    axios.get(`${process.env.REACT_APP_API_URL}/PullData`)
+      .then((response) => {
+        const data = response.data;
+        const Country_name = data.map((entry) => entry["Country name"]);
+        const populations = data.map((entry) => parseInt(entry["Population"]));
+        setChartData((prevData) => ({
+          ...prevData,
+          series: [{ data: populations }],
+          options: {
+            ...prevData.options,
+            xaxis: { categories: Country_name },
+          },
+        }));
+      })
+      .catch((error) => {
+        console.error("เกิดข้อผิดพลาดในการดึงข้อมูล:", error);
+      });
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Row gutter={[24,24]}>
+      <Col xs={24}>
+        <p>Population growth per country, 1950 to 2021</p>
+        <p>Click on the legend below to filter by continent 👇</p>
+      </Col>
+      <Col xs={24}>
+        <ReactApexChart
+          options={chartData.options}
+          series={chartData.series}
+          type="bar"
+          height={350}
+        />
+      </Col>
+    </Row>
   );
-}
+};
 
 export default App;
