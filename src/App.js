7397236c8d9471b171d2e21e36863b2ede437739
Udomsark Amnuaysookwong <udomsark1863@hotmail.com>
2023-11-14 15:59:05 +0700

    update 2023-11-14

--- src/App.js
+++ src/App.js
@@ -108,38 +108,60 @@ const App = () => {
   }, [year, fetchingData]);
   return (
     <Row gutter={[24, 24]}>
-      <Col xs={24} className="textMid">
+      <Col xs={24} >
         <h1>Population growth per country, 1950 to 2021</h1>
         <h2>Click on the legend below to filter by continent 👇</h2>
       </Col>
       <Col xs={24}>
-        <Card bordered={true} className="CardConfig">
-          <ReactApexChart
-            options={chartData.options}
-            series={chartData.series}
-            type="bar"
-            height={350}
+      <iframe
+        src="https://flo.uri.sh/visualisation/15739165/embed"
+        title="Interactive or visual content"
+        className="flourish-embed-iframe"
+        frameBorder="0"
+        scrolling="no"
+        style={{ width: "100%", height: "600px" }}
+        sandbox="allow-same-origin allow-forms allow-scripts allow-downloads allow-popups allow-popups-to-escape-sandbox allow-top-navigation-by-user-activation"
+      ></iframe>
+      <div
+        style={{
+          width: "100%",
+          marginTop: "4px",
+          textAlign: "right",
+        }}
+      >
+        <a
+          className="flourish-credit"
+          href="https://public.flourish.studio/visualisation/15739165/?utm_source=embed&utm_campaign=visualisation/15739165"
+          target="_top"
+          style={{ textDecoration: "none" }}
+        >
+          <img
+            alt="Made with Flourish"
+            src="https://public.flourish.studio/resources/made_with_flourish.svg"
+            style={{
+              width: "105px",
+              height: "16px",
+              border: "none",
+              margin: "0",
+            }}
           />
-        </Card>
-      </Col>
-      <Col xs={24} md={12} className="align-justify-center">
-        {" "}
-        <Button onClick={toggleFetching} className="textMid radiusO On-OffBTN">
-          {fetchingData ? (
-            <i class="align-justify-center fi fi-sr-pause"></i>
-          ) : (
-            <i class="align-justify-center fi fi-sr-play"></i>
-          )}
-        </Button>
-      </Col>
-      <Col xs={24} md={12} className="align-justify-center textRight">
-        <div className="TotalPopulation">
-          <h1>{year}</h1>
-          <h2>Total : {totalPopulation.toLocaleString()}</h2>
-        </div>
+        </a>
+      </div>
       </Col>
     </Row>
   );
 };
 
 export default App;
+
+/*
+import React from "react";
+import { Col } from "antd";
+
+const App = () => {
+  return (
+    <Col xs={24}>
+      
+    </Col>
+
+*/
